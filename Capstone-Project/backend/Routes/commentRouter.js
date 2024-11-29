e035b309581d48937fad0d709735f3673f9a7d32
190032016-srinivas <74003608+190032016-srinivas@users.noreply.github.com>
2024-11-29 15:48:58 +0530

    authentication middleware

--- Capstone-Project/backend/Routes/commentRouter.js
+++ Capstone-Project/backend/Routes/commentRouter.js
@@ -1,4 +1,19 @@
 import { validComments } from "../Models/comments.js";
+import jwt from "jsonwebtoken";
+function authenticateUser(req, res, next) {
+  const header = req.headers["authorization"];
+  if (!header || !header.startsWith("Bearer")) {
+    return res.status(403).json({ message: "no auth token provided" });
+  }
+  const token = header.split(" ")[1];
+  jwt.verify(token, "Srinivas_Secret_Key", (err, payload) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid auth token" });
+    } else {
+      next();
+    }
+  });
+}
 
 export function commentRouter(server) {
   server.get("/comments/:videoId", async (req, res) => {
@@ -8,13 +23,13 @@ export function commentRouter(server) {
       comments: response,
     });
   });
-  server.post("/comments/add", async (req, res) => {
+  server.post("/comments/add", authenticateUser, async (req, res) => {
     const comment = req.body;
     const newComment = await validComments.create(comment);
     if (!newComment) res.status(400).json({ message: "something went wrong" });
     else res.status(200).json({ comment: newComment });
   });
-  server.put("/comments/update", async (req, res) => {
+  server.put("/comments/update", authenticateUser, async (req, res) => {
     const { commentId, newComment } = req.body;
     const result = await validComments.updateOne(
       { _id: commentId },
@@ -26,11 +41,15 @@ export function commentRouter(server) {
     } else
       res.status(200).json({ message: "updated successfully ", newComment });
   });
-  server.delete("/comments/delete/:commentId", async (req, res) => {
-    const commentId = req.params.commentId;
-    const deleteComment = await validComments.deleteOne({ _id: commentId });
-    if (deleteComment.deletedCount === 0) {
-      return res.status(400).json({ message: "comment not found " });
-    } else res.status(200).json({ message: "deleted successfully " });
-  });
+  server.delete(
+    "/comments/delete/:commentId",
+    authenticateUser,
+    async (req, res) => {
+      const commentId = req.params.commentId;
+      const deleteComment = await validComments.deleteOne({ _id: commentId });
+      if (deleteComment.deletedCount === 0) {
+        return res.status(400).json({ message: "comment not found " });
+      } else res.status(200).json({ message: "deleted successfully " });
+    }
+  );
 }
